6818d5d035501ca414fab85c341380aff7166ac0
licheng <18892080039@163.com>
2021-02-18 18:20:15 +0800

    导出字符串处理

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const fs = require('fs')
-fs.readFile('./dev.txt', 'utf-8', function (err, data) {
+fs.readFile('./dev.text', 'utf-8', function (err, data) {
     if (err) {
         console.log(err)
     } else {
-        const originStr = replace(String(data))
-        console.log(originStr)
-        // const strList = originStr.split('!')
-        // console.log(strList)
-        // strList.map(ele => {
-        //     let obj ={}
-
-
-        // });
-
-
+        const originStr = replace(data)
+        const originArray = originStr.slice(0,originStr.length-1).split("!")
+        const array = originArray.map(ele => JSON.parse(ele));
+        
+        fs.writeFile('sum.xlsx','1234',function(error){
+            console.log(error)
+        })
     }
 })
 
 function replace(data) {
-    return data.replace(/Dever/g, "'Dever'").replace(/added lines/g, "'added lines'").replace(/removed lines/g, "'removed lines'").replace(/total lines/g, "'total lines'")
+    return data.replace(/Dever/g, '"Dever"').replace(/added lines/g, '"added lines"').replace(/removed lines/g, '"removed lines"').replace(/total lines/g, '"total lines"').replace(/\\n/g,'},').replace(/\\t/g,"").replace(/'/g,'"')
 }
+
+// git log --since=2020-12-01 --until="2020-12-31 23:59:59" --format='%aN' | sort -u | while read name; do echo -en "{Dever: '"$name"',"; git log --since=2020-12-01 --until="2020-12-31 23:59:59"  --author="$name" --pretty=tformat: --numstat | awk '{ add += $1;subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines: %s, total lines: %s}!", +add, +subs, +loc }' -; done  >> ./codeSum.text
+
---
+++ utils/index.js
@@ -0,0 +1,34 @@
+downloadExcell(data) {
+    //输出base64编码
+    function base64(s) {
+        return window.btoa(unescape(encodeURIComponent(s)))
+    }
+    //列标题
+    let str =
+        '<tr><td>高校名称</td><td>高校英文名称</td><td>国家地区</td><td>国家地区英文</td></tr>'
+    //循环遍历，每行加入tr标签，每个单元格加td标签
+    for (let i = 0; i < data.length; i++) {
+        str += '<tr>'
+        str += `<td>${data[i]['universityNameZN'] + '\t'}</td><td>${data[i][
+            'universityNameEN'
+        ] + '\t'}</td><td>${data[i]['regionNameZN'] + '\t'}</td><td>${data[i][
+            'regionNameEN'
+        ] + '\t'}</td>`
+        str += '</tr>'
+    }
+    //Worksheet名
+    let worksheet = 'Sheet1'
+    let uri = 'data:application/vnd.ms-excel;base64,'
+
+    //下载的表格模板数据
+    let template = `<html xmlns:o="urn:schemas-microsoft-com:office:office"
+xmlns:x="urn:schemas-microsoft-com:office:excel"
+xmlns="http://www.w3.org/TR/REC-html40">
+<head><meta charset='UTF-8'><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>
+<x:Name>${worksheet}</x:Name>
+<x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet>
+</x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->
+</head><body><table>${str}</table></body></html>`
+    //下载模板
+    window.location.href = uri + base64(template)
+},
